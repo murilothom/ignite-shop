1f4c2fe2a1417dbb7541abb1d2cbd7d19d9af8b7
Murilo Thom <murilothom.2002@hotmail.com>
2023-07-10 12:52:00 -0300

    feat: global styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,28 @@
+import { styled } from "../styles"
+
+const Button = styled('button', {
+  backgroundColor: '$green500',
+  borderRadius: 8,
+  width: '8rem',
+  height: '8rem',
+
+  span: {
+    fontWeight: 'bold'
+  },
+
+  '&:hover': {
+    filter: 'brightness(0.8)'
+  }
+})
+
 export default function Home() {
   return (
-    <h1>Hello World</h1>
+    <div>
+      <h1>Hello World</h1>
+      <Button>
+        <span>Teste</span>
+        Botão
+      </Button>
+    </div>
   )
 }
