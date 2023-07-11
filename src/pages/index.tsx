34da4702df214f9c4e4ca490cdb9226ce2353d81
Murilo Thom <murilothom.2002@hotmail.com>
2023-07-11 14:13:47 -0300

    feat: home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,28 +1,30 @@
-import { styled } from "../styles"
+import Image from "next/image"
+import { HomeContainer, Product } from "../styles/pages/home"
 
-const Button = styled('button', {
-  backgroundColor: '$green500',
-  borderRadius: 8,
-  width: '8rem',
-  height: '8rem',
-
-  span: {
-    fontWeight: 'bold'
-  },
-
-  '&:hover': {
-    filter: 'brightness(0.8)'
-  }
-})
+import camiseta1 from '../assets/camisetas/1.png'
+import camiseta2 from '../assets/camisetas/2.png'
+import camiseta3 from '../assets/camisetas/3.png'
 
 export default function Home() {
   return (
-    <div>
-      <h1>Hello World</h1>
-      <Button>
-        <span>Teste</span>
-        Botão
-      </Button>
-    </div>
+    <HomeContainer>
+      <Product>
+        <Image src={camiseta1} width={520} height={480} alt="" />
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+
+      <Product>
+        <Image src={camiseta2} width={520} height={480} alt="" />
+
+        <footer>
+          <strong>Camiseta Y</strong>
+          <span>R$ 99,90</span>
+        </footer>
+      </Product>
+    </ HomeContainer>
   )
 }
